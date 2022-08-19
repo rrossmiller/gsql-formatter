3f35d24a7180c56a9ab7e020cec6184d16be0d18
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-08-19 16:59:49 -0400

    save data

--- src/main.go
+++ src/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func greet() {
-	fmt.Printf( "Hello World! %v\n", time.Now())
+	fmt.Printf("Hello World! %v\n", time.Now())
+	b, _ := os.ReadFile("../test-data/simple.gsql")
+
+	fmt.Printf("b: %v\n", string(b))
 }
 
 func main() {
