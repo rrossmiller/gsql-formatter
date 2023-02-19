0c4bf3abb1af8f4c70c88b39a7564d77c94574ba
Rob Rossmiller <rrossmiller3@gmail.com>
2023-02-19 00:52:52 -0500

    start query body

--- tree-sitter-gsql/grammar.js
+++ tree-sitter-gsql/grammar.js
@@ -31,15 +31,17 @@ module.exports = grammar({
 
 		parameter_list: ($) => seq(
 			'(',
-			optional(repeat($.query_params)),
+			repeat($.query_params),
 			')'
 		),
+
 		query_params: $ => seq(
 			$.query_param,
 			optional(
 				repeat(seq(',', $.query_param))
 			)
 		),
+
 		query_param: ($) => seq(
 			$._type,
 			$.name,
@@ -51,7 +53,7 @@ module.exports = grammar({
 				'{',
 				optional(seq(repeat($.typedef), ';')),
 				// optional($.declaration_except_stmts),
-				// optional($.query_body_stmts),
+				$.query_body_stmts,
 				'}'
 			),
 
@@ -66,10 +68,8 @@ module.exports = grammar({
 
 		tuple_fields: $ => seq(
 			$.tuple_field,
-			optional(
-				repeat(
-					seq(',', $.tuple_field)
-				)
+			repeat(
+				seq(',', $.tuple_field)
 			)
 		),
 
@@ -78,6 +78,109 @@ module.exports = grammar({
 			seq($.name, $.base_type)
 		),
 
+		query_body_stmts: $ => seq(
+			$.query_body_stmt,
+			';'
+		),
+
+		query_body_stmt: $ => choice(
+			// !bookmark
+			$.assign_stmt,
+			$.v_set_var_decl_stmt
+			// declStmts 
+			// lAccumAssignStmt 
+			// gAccumAssignStmt 
+			// gAccumAccumStmt 
+			// funcCallStmt 
+			// selectStmt 
+			// queryBodyCaseStmt 
+			// queryBodyIfStmt 
+			// queryBodyWhileStmt 
+			// queryBodyForEachStmt 
+			// "BREAK" 
+			// "CONTINUE" 
+			// updateStmt 
+			// insertStmt 
+			// queryBodyDeleteStmt 
+			// printStmt 
+			// printlnStmt 
+			// logStmt 
+			// returnStmt 
+			// raiseStmt 
+			// tryStmt 
+		),
+
+		assign_stmt: $ => choice(
+			seq($.name, "=", $.expr),
+			seq($.name, ".", $.name, "=", $.expr)
+		),
+
+		v_set_var_decl_stmt: $ => seq(
+			// vertexSetName
+			$.name,
+			optional(
+				seq("(", $.name, ")")
+			),
+			"=",
+			choice($.seed_set)//, $.simple_set, $.select_stmt)
+		),
+
+		seed_set: $ => seq(
+			"{",
+			optional(
+				seq($.seed, repeat(seq(",", $.seed)))
+			),
+			"}"
+		),
+
+		seed: $ => choice(
+			'_',
+			"ANY",
+			// vertexSetName,
+			// globalAccumName,
+			// vertexType ".*",
+			// paramName,
+			// "SelectVertex" selectVertParams 
+		),
+
+		global_accum_name: $ => seq(
+			'@@',
+			$.name
+		),
+
+		local_accum_name: $ => seq(
+			'@',
+			$.name
+		),
+
+		arg_list: $ => seq(
+			$.expr,
+			repeat(seq(",", $.expr))
+		),
+
+		// ------
+		expr: $ => choice(
+			$.name,
+			$.global_accum_name,
+			seq($.name, ".", $.name),
+			seq($.name, ".", $.local_accum_name),//what's this -> ("\'")? ),
+			seq($.name, ".", $.name, ".", $.name, "(", optional($.arg_list), ")"),
+			// name "." name "(" (argList)? ")" ( "." "FILTER" "(" condition ")" )? 
+			// name ("<" type ("," type)* ">")? "(" (argList)? ")" 
+			// name "." localAccumName ("." name "(" (argList)? ")")+ ("." name)? 
+			// globalAccumName ("." name "(" (argList)? ")")+ ("." name)? 
+			// "COALESCE" "(" (argList)? ")" 
+			// aggregator "(" ("DISTINCT")? setBagExpr ")" 
+			// "ISEMPTY" "(" setBagExpr ")" 
+			// expr mathOperator expr 
+			// "-" expr 
+			// "(" expr ")" 
+			// "(" argList "->" argList ")" 
+			// "[" argList "]" 
+			// constant 
+			// setBagExpr 
+			// name "(" argList ")",
+		),
 		_type: $ => choice(
 			$.base_type,
 		),
