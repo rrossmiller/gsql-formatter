b2687089a7bb18180beb7d9fe10a8a9778adde2e
Rossmiller, Robert <robert.rossmiller@optum.com>
2023-03-07 17:24:11 -0500

    rm ex. consolidate bracketHandler. ready to start walking tree

--- ex/run.sh
+++
@@ -1,6 +0,0 @@
-clear
-npm run start &&
-	rm result.dot
-
-# dot -Tpng result.dot > result.png &&
-#     open result.png
--- fmt/index.ts
+++ fmt/index.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
-import Parser from 'tree-sitter';
+import Parser, {SyntaxNode} from 'tree-sitter';
 import {Tree} from 'tree-sitter';
 import gsql from 'tree-sitter-gsql';
-import {keywords} from './keywords';
-console.log('start');
+import {accumTypes, keywords} from './keywords';
 
 const parser = new Parser();
 parser.setLanguage(gsql);
@@ -13,35 +12,63 @@ let sourceCode = fs.readFileSync('../example.gsql', 'utf8');
 // capitalize keywords for parser
 let srcCodeCaps = '';
 let isComment = false;
-sourceCode.split('\n').forEach((l) => {
-    // separate comments to be able to find keywords before // with no space
-    if (l.includes('//')) {
-        l = l.replace('//', ' //');
+const whitespaceRegex = /^\s+$/;
+
+const bracketHandler = (line: string, seq: string): string => {
+    if (line.includes(seq)) {
+        const idx = line.indexOf(seq);
+        // if the comment doesn't start the line
+        // and there isn't already a space before
+        if (idx > 0 && line.charAt(idx - 1) !== ' ') {
+            line = line.replace(seq, ` ${seq}`);
+        }
     }
+    return line;
+};
+function handleNamesByBrakets(line: string): string {
+    // separate symbols to be able to find keywords before // with no space
+    line = bracketHandler(line, '//');
+    line = bracketHandler(line, '(');
+    line = bracketHandler(line, ')');
+    line = bracketHandler(line, '<');
+    line = bracketHandler(line, '>');
+    line = bracketHandler(line, '{');
+    line = bracketHandler(line, '}');
+    return line;
+}
 
-    srcCodeCaps +=
-        l
-            .split(' ')
-            .map((w: string) => {
-                if (w === '//') {
-                    isComment = true;
-                }
-                // ignore capitalization rule in a comment
-                if (!isComment) {
-                    const t = w.toUpperCase();
-                    if (keywords.includes(t)) {
-                        return t;
+sourceCode.split('\n').forEach((l) => {
+    // if the line is more than just whitespace
+    if (!whitespaceRegex.test(l)) {
+        l = handleNamesByBrakets(l);
+        srcCodeCaps +=
+            l
+                .split(' ')
+                .map((w: string) => {
+                    if (w === '//') {
+                        isComment = true;
                     }
-                }
-                return w;
-            })
-            .join(' ') + '\n';
-
+                    // ignore capitalization rule in a comment
+                    if (!isComment) {
+                        const t = w.toUpperCase();
+                        if (keywords.includes(t)) {
+                            return t;
+                        } else if (Object.keys(accumTypes).includes(t)) {
+                            return accumTypes[t];
+                        }
+                    }
+                    return w;
+                })
+                .join(' ') + '\n';
+    }
+    // if the line is just whitespace, remove it, but leave the line
+    else {
+        srcCodeCaps += '\n';
+    }
     isComment = false;
 });
 
-console.log(srcCodeCaps);
-
+fs.writeFileSync('testOut.gsql', srcCodeCaps);
 /* traverse -->
     use the visitor pattern https://craftinginterpreters.com/representing-code.html#the-visitor-pattern
     https://www.typescriptlang.org/docs/handbook/2/classes.html#implements-clauses
@@ -62,8 +89,8 @@ tree.rootNode.children.forEach((e) => {
 //     const cursor = tree.walk();
 //     let reachedRoot = false;
 //     let currentNode: SyntaxNode;
-
 //     while (!reachedRoot) {
+//         console.log(cursor.currentFieldName);
 //         currentNode = cursor.currentNode;
 //         if (
 //             // currentNode.childCount == 0 || !
