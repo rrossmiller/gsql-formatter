8ef3900a5c845ed2d48fdeeeb5e101c1664428a4
Rob Rossmiller <rrossmiller3@gmail.com>
2024-03-16 16:13:00 -0400

    glbl accum assign

--- src/gfmt/queryBodyStmts/accumAssign.go
+++ src/gfmt/queryBodyStmts/accumAssign.go
@@ -7,7 +7,7 @@ import (
 	sitter "github.com/smacker/go-tree-sitter"
 )
 
-func localAccumAssign(node *sitter.Node, src []byte) string {
+func accumAssign(node *sitter.Node, src []byte) string {
 	var sb strings.Builder
 	sb.WriteString(util.GetIndent(1))
 	for i := 0; i < int(node.ChildCount()); i++ {
--- src/gfmt/queryBodyStmts/queryBodyStmts.go
+++ src/gfmt/queryBodyStmts/queryBodyStmts.go
@@ -21,14 +21,12 @@ func QueryBodyStmts(node *sitter.Node, src []byte) string {
 			txt = assignStmt(child, src)
 		case "v_set_var_decl_stmt":
 			txt = vSetDecl(child, src)
-		case "l_accum_assign_stmt":
-			txt = localAccumAssign(child, src)
-		case "g_accum_assign_stmt":
-			txt = util.GetNodeText(child, src)
+		case "l_accum_assign_stmt", "g_accum_assign_stmt":
+			txt = accumAssign(child, src)
 		case "g_accum_accum_stmt":
 			txt = util.GetNodeText(child, src)
 		case "func_call_stmt":
-			txt = funcStmt(child, src,1)
+			txt = funcStmt(child, src, 1)
 		case "gsql_select_block":
 			txt = util.GetNodeText(child, src)
 		case "query_body_case_stmt":
